9173997038b9c76da4a05b0973cf8477b289b45d
nalhamzy <63153023+nalhamzy@users.noreply.github.com>
2021-11-02 22:40:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
 
 
 
-    sentences = st.text_area("sentences", height=300)
+    sentences = st.text_area("Paste sentences here:", height=300)
 
     if sentences != None:
         sentences = sentences.splitlines()
